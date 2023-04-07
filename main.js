3ab7ada22e3bfff59e096493d7264e082ffd29db
JayashreeSelvarangam <116268739+JayashreeSelvarangam@users.noreply.github.com>
2023-04-07 16:02:54 +0530

    added the default products

--- main.js
+++ main.js
@@ -253,6 +253,47 @@ if(!product_detail_list){
         },
         "currency": "Rs.",
         "product_id": uuidv4()
+     }, {
+        "category": 'ESPRESSO',
+        // "category_description":category_description,
+        // "description":description,
+        "name":  'Java chip ',
+        "quantities": {
+           'tall':{
+           "name": 'tall',
+           "quantity": 550,
+           "unit": "ml",
+           "calories": 255,
+           "calories_unit": "kcal"},
+           'medium':{
+           "name": 'medium',
+           "quantity": 250,
+           "unit": "ml",
+           "calories": 155,
+           "calories_unit": "kcal"
+           },
+           'short':{
+           "name": 'short',
+           "quantity": 155,
+           "unit": "ml",
+           "calories": 75,
+           "calories_unit": "kcal"
+           }
+        },
+        "image": {
+           "source": "../../assets/image/vanillacold.jpg ",
+           "alt": ""
+        },
+
+        "description": 'fresh tea',
+        'price':cost,
+        "price_1":{
+           'tall':200,
+           'medium':100,
+           'short':70
+        },
+        "currency": "Rs.",
+        "product_id": uuidv4()
      }
  ]
 }
